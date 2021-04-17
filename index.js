f25844ea13a09d95a467d2ef02ca3356fbeb254c
Ved Rane <>
2021-04-17 03:24:17 +0000

    Fixed Cross-site referencing error

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const os = require("os");
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const fs = require("fs");
@@ -6,30 +8,34 @@ const io = require("socket.io");
 
 
 app.use(express.static('public'));
-
 app.listen(3000);
 
 app.use(bodyParser.urlencoded({
    extended: false
 }));
-
+app.use(cors({
+  origin: 'https://wispkoe.repl.co/'
+}));
 app.use(bodyParser.json());
- 
+
 app.post('/new-post', function (req, res) {
+   
+   if(req.get('referer') == 'https://29e5be10-b642-4d0a-9da1-ffb715d4c97c.id.repl.co/'){
+       var post = JSON.stringify(req.body);
   
-  var post = JSON.stringify(req.body);
-  
-  res.redirect(req.get('referer'));
+  res.redirect("https://wispkoe.repl.co");
   
   fs.readFile("public/posts.json", function (err, data) {
     
     var json = JSON.parse(data);
     json.push(post);
     
-    fs.writeFile("public/posts.json", JSON.stringify(json), function(err, result){ if(err) console.log('error', err);
-    });
+    fs.writeFile("public/posts.json", JSON.stringify(json), function(err, result){ if(err) console.log('error', err)
+  
+  });
+  });
+   } else {
+     res.end("Error");
+   }
     
   });
-  
-  
-});
